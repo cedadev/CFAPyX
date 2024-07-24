b8c2ed7305a11772065a4cead8435d0e67bde3db
dwest77 <daniel.westwood@stfc.ac.uk>
2024-07-24 09:02:52 +0100

    Removed for the notes branch

--- tests/testDomain.py
+++
@@ -1,7 +0,0 @@
-import cf
-
-pwd = '/'.join(__file__.split('/')[:-2]) + '/testfiles'
-print(f'Opening files under {pwd}')
-f = cf.read(f'{pwd}/huss*')
-
-print(dir(f))
--- xarray_repos/fsspec_test.py
+++
@@ -1,7 +0,0 @@
-import fsspec as fs
-import xarray as xr
-
-kfile = 'https://dap.ceda.ac.uk/badc/cmip6/metadata/kerchunk/pipeline1/CMIP/AS-RCEC/TaiESM1/kr1.0/CMIP6_CMIP_AS-RCEC_TaiESM1_historical_r1i1p1f1_3hr_clt_gn_v20201013_kr1.0.json'
-
-mapper = fs.get_mapper('reference://',fo=kfile, backend_kwargs={'compression':None}, remote_protocol='https')
-ds = xr.open_zarr(mapper, consolidated=False, decode_times=True)

6f02e2597882bbe59d86cbaaef1d3986365c3106
dwest77 <daniel.westwood@stfc.ac.uk>
2024-08-08 11:32:33 +0100

    Added copywright to group

--- CFAPyX/group.py
+++ CFAPyX/group.py
@@ -1,3 +1,7 @@
+__author__    = "Daniel Westwood"
+__contact__   = "daniel.westwood@stfc.ac.uk"
+__copyright__ = "Copyright 2023 United Kingdom Research and Innovation"
+
 from contextlib import suppress
 
 class VariableWrapper:

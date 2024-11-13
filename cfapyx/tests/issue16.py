fb19b4361f386824c7a496c330b57aeffe423951
dwest77 <daniel.westwood@stfc.ac.uk>
2024-11-13 14:37:26 +0000

    Migrated issue

--- cfapyx/tests/issue16.py
+++
@@ -1,7 +0,0 @@
-import xarray as xr
-
-file = '/gws/nopw/j04/cedaproc/dwest77/CMIP6_ScenarioMIP_CNRM-CERFACS_CNRM-ESM2-1_ssp119_r1i1p1f2_3hr_huss_gr_v20190328_cfa1.0.nca'
-
-ds = xr.open_dataset(file, engine='CFA', cfa_options={'decode_cfa':False})
-x = 1
-ds = xr.open_dataset(file)

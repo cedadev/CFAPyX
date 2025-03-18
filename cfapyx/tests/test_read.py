fd9802915300a0a1eb109aaf897a5793fe318216
dwest77a <daniel.westwood@stfc.ac.uk>
2025-03-18 17:25:19 +0000

    Removed chunked tests - active storage not a dependency anymore

--- cfapyx/tests/test_read.py
+++ cfapyx/tests/test_read.py
@@ -31,54 +31,17 @@ class TestCFARead:
         assert p_sel.shape == (3, 5, 10)
         assert not hasattr(p_sel, 'aggregated_data')
         assert not hasattr(p_sel, 'aggregated_dimensions')
-
         p_mean = p_sel.mean(dim='time')
 
         assert p_mean.shape == (5, 10)
-        assert (p_mean[0][0].to_numpy() - 0.635366) < 0.01
+        assert abs(p_mean[0][0].to_numpy() - 0.635366) < 0.01, "Pure Data Invalid"
 
         p_value = p_sel.mean()
 
         assert p_value.shape == ()
-        assert (p_value.to_numpy() - 0.511954) < 0.01
+        assert abs(p_value.to_numpy() - 0.511954) < 0.01, "Pure Data Invalid"
 
         print('Integration tests: Read(pure) - complete')
-
-    def test_cfa_chunks(self, testdir=TESTDIR):
-
-        FILE = f'{testdir}/testrain.nca'
-
-        ds = xr.open_dataset(FILE, engine='CFA',
-                            cfa_options={
-                                'chunks': {'longitude':180},
-                                'chunk_limits':False})
-        
-        ## Test global dataset
-        assert not hasattr(ds,'address')
-        assert not hasattr(ds,'shape')
-        assert not hasattr(ds,'location')
-        
-        assert 'p' in ds
-        assert ds['p'].shape == (20, 180, 360)
-
-        # Using selection with lat/lon values NOT index values
-        p_sel = ds['p'].isel(time=slice(0,3),latitude=slice(140,145), longitude=slice(100,300))
-
-        assert p_sel.shape == (3, 5, 200)
-        assert not hasattr(p_sel, 'aggregated_data')
-        assert not hasattr(p_sel, 'aggregated_dimensions')
-
-        p_mean = p_sel.mean(dim='time')
-
-        assert p_mean.shape == (5, 200)
-        assert (p_mean[0][0].to_numpy() - 0.664414) < 0.01
-
-        p_value = p_sel.mean()
-
-        assert p_value.shape == ()
-        assert (p_value.to_numpy() - 0.490389) < 0.01
-
-        print('Integration tests: Read(chunked) - complete')
 
 if __name__ == '__main__':
 
@@ -86,6 +49,5 @@ if __name__ == '__main__':
     #os.chdir('CFAPyX')
 
     TestCFARead().test_cfa_pure()
-    TestCFARead().test_cfa_chunks()
 
     print('Run with poetry run pytest -v -s')
